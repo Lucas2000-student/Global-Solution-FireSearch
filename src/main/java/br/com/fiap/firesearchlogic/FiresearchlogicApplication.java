fd99bab96365dd56e22bdfd4c75ce18665d90537
Lucas2000-student <rm560179@fiap.com.br>
2026-06-02 21:23:31 -0300

    Subindo estrutura do projeto.

---
+++ src/main/java/br/com/fiap/firesearchlogic/FiresearchlogicApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.firesearchlogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiresearchlogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiresearchlogicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/firesearchlogic/FiresearchlogicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.firesearchlogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiresearchlogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
